9dc064ef32a68c8f4b96dcc5866ae7467974d774
Pierre Ballif <pierre@ballif.eu>
2026-04-23 16:24:19 +0200

    fix: always find package in make docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,13 @@
 project = "vector-structure"
 author = "Pierre Ballif <pierre@ballif.eu>"
 
+# Find package even if it is not installed
+# fmt: off
+import sys  # noqa: E402, I001
+from pathlib import Path  # noqa: E402, I001
+sys.path.append(str(Path(__file__).parent.parent.parent))
+# fmt: on
+
 extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
